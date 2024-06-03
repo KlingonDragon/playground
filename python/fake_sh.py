c5b2cdde9c732e58bdfe0b24b3357c82cb80ab55
klingondragon <klingondragon@hotmail.co.uk>
2024-06-03 18:34:26 +0100

    fake shell

---
+++ python/fake_sh.py
@@ -0,0 +1,19 @@
+from os import environ, getcwd, name, uname
+from sys import argv
+from random import choice
+
+
+GREEN = "\033[0;32m"
+BLUE = "\033[0;34m"
+RESET = "\033[0m"
+RUDE_MSGS = ("No. I don't like you", "Piss Off", "Leave me alone")
+for _ in range(int(argv[1]) if len(argv) > 1 else 10):
+    try:
+        input(
+            f"{getcwd()}> "
+            if name == "nt"
+            else f"{GREEN}{environ['USER']}@{uname().nodename}:{BLUE}{environ['PWD'].replace(environ['HOME'], '~')}{RESET}$ "
+        )
+        print(choice(RUDE_MSGS))
+    except KeyboardInterrupt:
+        print("\nYou can't escape that easily.")
