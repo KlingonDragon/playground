b430cf48524b65235a1936e08b7ace8930a2b8c3
klingondragon <klingondragon@hotmail.co.uk>
2024-12-24 15:01:00 +0000

    New rude message

--- python/fake_sh.py
+++ python/fake_sh.py
@@ -6,7 +6,12 @@ from random import choice
 GREEN = "\033[0;32m"
 BLUE = "\033[0;34m"
 RESET = "\033[0m"
-RUDE_MSGS = ("No. I don't like you", "Piss Off", "Leave me alone")
+RUDE_MSGS = (
+    "No. I don't like you",
+    "Piss Off",
+    "Leave me alone",
+    "Go Away",
+)
 for _ in range(int(argv[1]) if len(argv) > 1 else 10):
     try:
         input(
