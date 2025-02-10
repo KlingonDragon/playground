407887177072eb87c231ddd614200a7fd356d945
klingondragon <klingondragon@hotmail.co.uk>
2025-02-10 21:06:02 +0000

    updated f1 teams and minor ts/css updates

--- ts-app/src/index.ts
+++ ts-app/src/index.ts
@@ -12,7 +12,7 @@ $('body', { classList: ['light'] })?._(
         })._('Hello World!'),
         _('input', { classList: ['dark'], properties: { type: 'password', placeholder: 'password?' } })
     ),
-    _('footer', { classList: ['dark'] })
+    _('footer', { classList: ['dark'] })._('Footer content')
 );
 setTimeout(() => $('#some-id', { classList: ['updated-test-class'], replaceClassList: true }), 3e3)
 setTimeout(() => console.log($('input', { properties: { value: 'Secret!' } })?.value), 8e3)
